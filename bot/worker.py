1f9561caa9cf09ba889cbe32add208df2b0faf8f
117080364+Niffy-the-conqueror@users.noreply.github.com <117080364+Niffy-the-conqueror@users.noreply.github.com>
2022-12-13 23:35:04 +0100

    trial #4

--- bot/worker.py
+++ bot/worker.py
@@ -188,7 +188,7 @@ async def listqueuep(event):
     await yo.delete()
 
 
-def encodestat():
+async def encodestat():
     if FCHANNEL and FCHANNEL_STAT:
         if not QUEUE and not WORKING:
             x = "**Currently Resting…😑**"
@@ -220,27 +220,22 @@ def encodestat():
         return x
 
 
-def stateditor(x, channel, id):
+async def stateditor(x, channel, id):
     try:
         if channel and id:
-            # return await app.edit_message_text(channel, id, x)
-            return app.edit_message_text(channel, id, x)
+            return await app.edit_message_text(channel, id, x)
 
     except Exception:
         pass
 
 
-def autostat():
+async def autostat():
     try:
         if FCHANNEL and FCHANNEL_STAT:
             while FCHANNEL_STAT:
-                # estat = await encodestat()
-                estat = encodestat()
-                # await stateditor(estat, int(FCHANNEL), int(FCHANNEL_STAT))
-                stateditor(estat, int(FCHANNEL), int(FCHANNEL_STAT))
-
-                # await asyncio.sleep(60)
-                time.sleep(60)
+                estat = await encodestat()
+                await stateditor(estat, int(FCHANNEL), int(FCHANNEL_STAT))
+                await asyncio.sleep(60)
     except Exception:
         pass
 
