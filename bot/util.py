83a383693710cdfae716aff9b1d8999e323edfaf
117080364+Niffy-the-conqueror@users.noreply.github.com <117080364+Niffy-the-conqueror@users.noreply.github.com>
2022-12-18 19:15:31 +0100

    syntax fix

--- bot/util.py
+++ bot/util.py
@@ -474,7 +474,7 @@ async def custcap(name, fname):
             caption += f"**{cdp} Episode Title:** `{st}`\n"
         if "1080" in nani:
             caption += "**{cdp} 🌟:** `[1080p] [AV1]`\n"
-        caption += f"{cdp} **CRC32:** `[{crc32s}]`\n"
+        caption += f"**{cdp} CRC32:** `[{crc32s}]`\n"
         caption += "**🔗 @ANi_MiNE**"
     except Exception:
         ers = traceback.format_exc()
