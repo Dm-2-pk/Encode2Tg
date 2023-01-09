f2a980067a06cc46a0825cd1cf0be88ca0af177a
117080364+Niffy-the-conqueror@users.noreply.github.com <117080364+Niffy-the-conqueror@users.noreply.github.com>
2023-01-09 02:56:15 +0100

    fix

--- bot/util.py
+++ bot/util.py
@@ -335,7 +335,7 @@ async def parse(name, kk, aa):
         bb = kk.replace(f".{aa}", " @Ani_Mine.mkv")
         bb2 = bb
     if "/" in bb:
-        bb = bb.replace("/", "")
+        bb = bb.replace("/", " ")
     return bb, bb2
 
 
